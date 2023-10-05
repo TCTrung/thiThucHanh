c46615b986a60c9d840ca686bc59e27a620147ab
TCTrung <trungtrinh1325@gmail.com>
2023-10-05 10:55:05 +0700

    baithi

---
+++ Cau4/Rectangle.js
@@ -0,0 +1,28 @@
+class Rectangle {
+    x;
+    y;
+    width;
+    height;
+    color;
+    ctx;
+
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.canvas = document.getElementById('canvas');
+        this.ctx = this.canvas.getContext('2d');
+
+    }
+
+    draw() {
+        this.ctx.beginPath();
+        this.ctx.rect(this.x, this.y, this.width, this.height);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
+
+}
